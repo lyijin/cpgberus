0dde1a9b4ca681d4a514d280e7dd252ca6d9c1d5
Liew, Yi Jin (H&B, North Ryde) <Yijin.Liew@csiro.au>
2021-08-06 13:50:30 +1000

    squashed bug in producing unsorted compiled tables

--- 04_parse_bismark_covs/tabulate_cov_coverages_methpct.py
+++ 04_parse_bismark_covs/tabulate_cov_coverages_methpct.py
@@ -23,6 +23,7 @@ Autodetects whether input files are gzip-compressed from `.gz` suffix.
 import argparse
 import gzip
 from pathlib import Path
+import subprocess
 import sys
 import time
 
@@ -74,6 +75,9 @@ for n, bc in enumerate(args.bismark_covs):
             bc_df[['scaffold', 'start_pos', 'end_pos', f'{bc_id}_methpct']],
             how='outer')
 
+if args.verbose:
+    benchmark_print(f'Union of all files produces {len(cov_df)} rows.')
+
 # irritatingly, anytime NaN appears, the column will be casted to np.float64
 # tidy up tables after the merging
 cov_df = cov_df.fillna(0)
@@ -88,12 +92,25 @@ methpct_cols = [x for x in methpct_df.columns if '_methpct' in x]
 methpct_df[methpct_cols] = methpct_df[methpct_cols].round(3).astype(str)
 methpct_df.columns = [x.replace('_methpct', '') for x in methpct_df.columns]
 
-if args.verbose:
-    benchmark_print(f'Union of all files produces {len(cov_df)} rows.')
-
 # print to designated output files
 if args.verbose: benchmark_print(f'Writing coverages out...')
-cov_df.to_csv('all.coverages.tsv.gz', sep='\t', index=False)
+cov_df.to_csv('all.coverages.tsv', sep='\t', index=False)
 
 if args.verbose: benchmark_print(f'Writing methylation percentages out...')
-methpct_df.to_csv('all.methpcts.tsv.gz', sep='\t', index=False)
+methpct_df.to_csv('all.methpcts.tsv', sep='\t', index=False)
+
+# during merging, columns are NOT sorted properly, especially during
+# outer joins where the second file contains "pos" not found in the first
+# natural sort in python is slow as, hence relying on linux sort (-V) to
+# achieve proper sorting in the scaffold column
+if args.verbose:
+    benchmark_print('Using Linux sort to sort output files properly...')
+
+for outf in ['all.coverages.tsv', 'all.methpcts.tsv']:
+    subprocess.call(f'cat {outf} | head -1 > tmpheader', shell=True)
+    subprocess.call(f'cat {outf} | sed 1d | sort -k1,1V -k2,2n > tmpbody', shell=True)
+    subprocess.call(f'cat tmpheader tmpbody | gzip > {outf}.gz', shell=True)
+    subprocess.call(f'rm -f tmpheader tmpbody {outf}', shell=True)
+
+if args.verbose:
+    benchmark_print('Done.')
