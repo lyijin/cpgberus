1bb34b9bfa7e1c5708d2763123514d032dda7329
Guanzon, Dominic (H&B, North Ryde) <Dominic.Guanzon@csiro.au>
2021-09-27 22:00:31 +1000

    Minor changes to stats script and renamed files. Cleaned exploration coverage script and also improved subsampling.
    - 01_coverage_and_sub_sampling.R: Cleaned up coverage code, further improvement to subsampling code to deal with 0.5 meth.
    - 01_coverage_and_sub_sampling.R: Output a granges list for stats script.

---
+++ 05_CpG_sequence_context/01_coverage_and_sub_sampling.R
@@ -0,0 +1,133 @@
+#####  Script information  #####
+################################
+
+# This code calculates coverage distribution and can also subsample using rbinom.
+# This information can be used to make a coverage cutoff before statistical analysis.
+# In addition, subsampling of high coverage samples can be performed before statistical analysis.
+
+#####  Dependencies  #####
+##########################
+
+library(GenomicRanges)
+library(ggplot2)
+library(scales)
+
+#####  Load data  #####
+#######################
+
+load("/scratch1/gua020/CpGberus_data/grch38p13_combined_covs_grl.RData")
+
+#####  Functions  #####
+#######################
+
+#######################################################################
+# # Funtion to plot either density or tally coverage distribution
+#######################################################################
+
+Coverage_distribution <- function(List_of_dataframes, density_or_tally, coverage_column_name, plot_name) {
+
+    n = names(List_of_dataframes)
+    
+    if (density_or_tally == "Density") {
+        # Calculate coverage density, then dataframe of x and y density coord + name
+        print("Calculating coverage densities")
+        temp_df = lapply(List_of_dataframes, function(x) density(x[, coverage_column_name], adjust = 5))
+        temp_df = lapply(n, function(n) data.frame(Sample_name = n, x_values = temp_df[[n]][["x"]], y_values = temp_df[[n]][["y"]]))
+    } else if (density_or_tally == "Tally") {
+        # Calculate tally coverage density, then dataframe of x and y density coord + name
+        print("Calculating coverage tallys")
+        temp_df = lapply(List_of_dataframes, function(x) data.frame(table(x[, coverage_column_name])))
+        temp_df = lapply(n, function(n) data.frame(Sample_name = n, x_values = as.integer(temp_df[[n]][["Var1"]]), y_values = temp_df[[n]][["Freq"]]))
+    }
+    
+    # Collapse list of dataframes
+    temp_df = do.call(rbind, temp_df)
+    
+    # Add sample name without suffix and sequencing type column
+    temp_df$Sample_name_2 = substr(temp_df$Sample_name, 1, nchar(temp_df$Sample_name) - 1)
+
+    temp_df$Seq_type = temp_df$Sample_name
+    temp_df$Seq_type[grep("E$", temp_df$Sample_name)] = "EM-Seq"
+    temp_df$Seq_type[grep("W$", temp_df$Sample_name)] = "WGBS"
+
+    # Plot
+    pdf(paste0(plot_name, ".pdf"), width = 11.69, height = 8.3)
+    plot(ggplot(temp_df, aes(x=x_values, y=y_values, group = Sample_name, color = Seq_type)) + geom_line() + scale_x_continuous(breaks = pretty_breaks(10)) + theme_bw() + xlab("Coverage") + ylab(density_or_tally))
+    plot(ggplot(temp_df, aes(x=x_values, y=y_values, group = Sample_name, color = Seq_type)) + geom_line() + scale_x_continuous(breaks = pretty_breaks(10), limits = c(-5, 70)) + theme_bw() + xlab("Coverage") + ylab(density_or_tally))
+    plot(ggplot(temp_df, aes(x=x_values, y=y_values, group = Sample_name, color = Sample_name)) + geom_line() + scale_x_continuous(breaks = pretty_breaks(10), limits = c(-5, 70)) + theme_bw() + xlab("Coverage") + ylab(density_or_tally))
+    plot(ggplot(temp_df, aes(x=x_values, y=y_values, group = Sample_name, color = Sample_name_2)) + geom_line(aes(linetype = Seq_type)) + scale_x_continuous(breaks = pretty_breaks(10), limits = c(-5, 70)) + theme_bw() + xlab("Coverage") + ylab(density_or_tally))
+    dev.off()
+    
+    return(temp_df)
+}
+
+#####  Analysis using density and tally  #####
+##############################################
+
+# Total coverage calculation, convert to dataframe
+Covs_grl_all_df = lapply(covs_grl, function(x) data.frame(chr = seqnames(x), pos = start(x), N = (x$meth_cov + x$unmeth_cov), X = x$meth_cov, cpg_context_nnncgnnn = x$cpg_context_nnncgnnn))
+rm(covs_grl)
+
+# Subset N (coverage) <= 100
+Covs_grl_all_df_subset = lapply(Covs_grl_all_df, function(x) x[x[, "N"] <= 100, ])
+
+# Calculate and plot coverage density, coverage filtered < 100 because density calculations are affected.
+Covs_grl_all_df_density = Coverage_distribution(Covs_grl_all_df_subset, "Density", "N", "Coverage_analysis_density")
+
+# Calculate and plot coverage tally without filtering for coverage
+Covs_grl_all_df_tally = Coverage_distribution(Covs_grl_all_df, "Tally", "N", "Coverage_analysis_tally")
+
+
+#####  Subsample and create granges list for stat analysis script  #####
+########################################################################
+
+# Subsample (using rbinom) high coverage data to be more equivalent to low coverage data.
+Covs_grl_all_df_subset = lapply(Covs_grl_all_df, function(x) cbind(x, data.frame(Subsample_coverage = x[, "N"])))
+
+set.seed(1)
+Covs_grl_all_df_subset[["WR069V1E"]]["Subsample_coverage"] = rbinom(nrow(Covs_grl_all_df_subset[["WR069V1E"]]), Covs_grl_all_df_subset[["WR069V1E"]][, "N"], 0.30)
+set.seed(1)
+Covs_grl_all_df_subset[["WR025V1E"]]["Subsample_coverage"] = rbinom(nrow(Covs_grl_all_df_subset[["WR025V1E"]]), Covs_grl_all_df_subset[["WR025V1E"]][, "N"], 0.55)
+set.seed(1)
+Covs_grl_all_df_subset[["WR025V9E"]]["Subsample_coverage"] = rbinom(nrow(Covs_grl_all_df_subset[["WR025V9E"]]), Covs_grl_all_df_subset[["WR025V9E"]][, "N"], 0.75)
+
+# Remove subsample_coverage = 0 and add metadata required for stats script.
+Covs_grl_all_df_subset = lapply(Covs_grl_all_df_subset, function(x) x[x[, "Subsample_coverage"] > 0, ])
+Covs_grl_all_df_subset = lapply(Covs_grl_all_df_subset, function(x) cbind(x, data.frame(meth_cov = x[, "X"], unmeth_cov = (x[, "N"] - x[, "X"]))))
+                                    
+# Correct meth / unmeth for subsampled data.
+Samples_to_correct = c("WR069V1E", "WR025V1E", "WR025V9E")
+
+for (data_name in Samples_to_correct) {
+    print(paste0("Correcting sample: ", data_name))
+    temp_data = Covs_grl_all_df_subset[[data_name]]
+    meth_cov = ceiling(temp_data$X / temp_data$N * temp_data$Subsample_coverage)
+    
+    # Randomise CpG meth_cov if decimal is x.5, indicating that could be meth or unmeth
+    pos_to_randomise = (meth_cov - (temp_data$X / temp_data$N * temp_data$Subsample_coverage)) == 0.5
+    pos_to_randomise[pos_to_randomise == TRUE] = 1
+    set.seed(1)
+    pos_to_randomise = rbinom(length(pos_to_randomise), pos_to_randomise, 0.5)
+    
+    # Subtract randomise CpG from meth_cov; 1 = unmeth, 0 = meth. Also calculate unmeth
+    meth_cov = meth_cov - pos_to_randomise
+    unmeth_cov = temp_data$Subsample_coverage - meth_cov
+    
+    # Replace list of dataframes
+    temp_data$meth_cov = as.integer(meth_cov)
+    temp_data$unmeth_cov = as.integer(unmeth_cov)
+    Covs_grl_all_df_subset[[data_name]] = temp_data
+}
+
+# Calculate and plot coverage tally without filtering for coverage.
+Covs_grl_all_df_tally_subsample = Coverage_distribution(Covs_grl_all_df_subset, "Tally", "Subsample_coverage", "Coverage_analysis_tally_subsample")
+
+# Convert to granges list for stats analysis.
+covs_grl_subsample = lapply(Covs_grl_all_df_subset, function(x) GRanges(seqnames = x$chr, ranges=IRanges(start = x$pos, end = (x$pos + 1)), 
+                            meth_cov = x$meth_cov, unmeth_cov = x$unmeth_cov, cpg_context_nnncgnnn = x$cpg_context_nnncgnnn))
+
+covs_grl_subsample = do.call("GRangesList", covs_grl_subsample)
+
+# Save workspace
+save(Covs_grl_all_df_density, Covs_grl_all_df_tally, Covs_grl_all_df_tally_subsample, covs_grl_subsample, file ="Data_coverage_filtered.RData")
+quit(save = "no")
--- 05_CpG_sequence_context/02_CpG_context_stats.R
+++ 05_CpG_sequence_context/02_CpG_context_stats.R
@@ -23,12 +23,12 @@ load("/scratch1/gua020/CpGberus_data/grch38p13_combined_covs_grl.RData")
 # # Function to merge CpGs which exist between all samples, fill empty with 0
 ################################################################################
 
-Merge_CpGs <- function(Dataframe_list_object) {
+Merge_CpGs <- function(Granges_list_object) {
 
     # Find existing chromosomes
     number_of_chromosomes = list()
     
-    for (data_frame in Dataframe_list_object) {
+    for (data_frame in Granges_list_object) {
         temp_list = as.character(unique(data_frame$chr))
         number_of_chromosomes = unlist(unique(c(number_of_chromosomes, temp_list)))
     }
@@ -39,7 +39,7 @@ Merge_CpGs <- function(Dataframe_list_object) {
     for (chr_num in number_of_chromosomes) {
         temp_pos = list()
         
-        for (data_frame in Dataframe_list_object) {
+        for (data_frame in Granges_list_object) {
             chr_num_pos = data_frame[(data_frame$chr == chr_num), "pos"]
             temp_pos = unique(unlist(c(temp_pos, chr_num_pos)))
         }
@@ -51,9 +51,9 @@ Merge_CpGs <- function(Dataframe_list_object) {
     # Compare sample CpGs to existing CpGs and fill differences with 0    
     final_dataframe_list = list()
     
-    for (dataframe_index in seq(length(Dataframe_list_object))) {
-        data_frame = Dataframe_list_object[[dataframe_index]]
-        data_frame_name = names(Dataframe_list_object[dataframe_index])
+    for (dataframe_index in seq(length(Granges_list_object))) {
+        data_frame = Granges_list_object[[dataframe_index]]
+        data_frame_name = names(Granges_list_object[dataframe_index])
         temp_df = data.frame(chr = factor(), pos = integer(), N = integer(), X = integer())
 
         for (chr_num in number_of_chromosomes) {
@@ -70,7 +70,7 @@ Merge_CpGs <- function(Dataframe_list_object) {
         temp_df = temp_df[order(temp_df$chr, temp_df$pos), ]
         rownames(temp_df) <- seq(1:nrow(temp_df))
         final_dataframe_list[[data_frame_name]] <- temp_df
-        print(paste0(data_frame_name, " data is finished merging: ", dataframe_index, " / ", length(Dataframe_list_object)))
+        print(paste0(data_frame_name, " data is finished merging: ", dataframe_index, " / ", length(Granges_list_object)))
     }
     return(final_dataframe_list)
 }
@@ -108,7 +108,7 @@ Create_BS_object <- function(Dataframe_list_object) {
 
 ##############################################################
 # # Function here to create smooth fit by chromsome
-
+#
 # This was created so that the BS smoothed values can be put into
 # DMLtest statistics function. DMLtest itself uses a smoothing
 # average algorithim before statistics, where smoothing can be 
@@ -137,7 +137,7 @@ Create_smooth_fit_list_per_chr <- function(bsseq_object, smoothing_span, number_
 
 ##############################################################
 # # Function here to perform stats by chromsome
-
+#
 # Approximately 1.2 gb per chromosome / worker (sample).
 # On one HPC node, could run theoretically 20 workers (20 cpu's).
 # Should be fine to run 20 samples per node (20gb needed approx).
@@ -186,12 +186,5 @@ Covs_grl_all_bsseq_list_stats = Create_stat_list_per_chr(Covs_grl_all_bsseq, smo
 Covs_grl_all_bsseq_list_stats_no_smooth = Create_stat_list_per_chr(Covs_grl_all_bsseq, smoothing_bool = FALSE, number_of_workers = 8, group_1 = sample_group_1, group_2 = sample_group_2)
 
 # Save workspace
-rm(sample_group_1)
-rm(sample_group_2)
-rm(Create_BS_object)
-rm(Create_smooth_fit_list_per_chr)
-rm(Create_stat_list_per_chr)
-rm(Merge_CpGs)
-
-save.image("Motif_stats.RData")
+save(Covs_grl_all_bsseq, Covs_grl_all_bsseq_list_stats, Covs_grl_all_bsseq_list_stats_no_smooth, file = "Motif_stats.RData")
 quit(save = "no")
